7db858935d9eebe3d2e181945a6479bae448eb0b
Nataliia Breslavets <breslavets_n@ukr.net>
2023-04-12 11:45:41 +0300

    task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,11 +4,11 @@
 //  виведе в консоль текст заголовку елемента (тегу <h2>) і 
 //  кількість елементів в категорії (усіх <li>, вкладених в нього).
 
-
+// const categoriesUl = document.querySelector('#categories')
 const categorEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categorEl.length}`);
 
-const titleEl = document.querySelectorAll('h2');
-console.log(titleEl);
-console.log(titleEl. textContent );
-// 
+categorEl.forEach((item=>{
+console.log(`Category: ${item.firstElementChild.textContent}`);
+console.log(`Elements: ${item.lastElementChild.children.length}`);
+}));
